a4dbb6743fe00f2f54260190aa57a97f9f13d5dc
Iuliusgh <81573719+Iuliusgh@users.noreply.github.com>
2023-03-16 15:13:15 +0100

    Revert "Merge branch 'main' of https://github.com/castrodaniel18/DVI"
    This reverts commit f35e5d1d28fbe404023e9e094bb740226ad6447d, reversing
    changes made to 61a98e5dab026dc2659f6f6e03f1db227117e1af.

--- src/scenes/Level1Scene.js
+++ src/scenes/Level1Scene.js
@@ -1,5 +1,4 @@
 import Phaser from "phaser";
-import Goblin from "../Goblin";
 import Player from "../objects/Player";
 
 export default class Level1Scene extends Phaser.Scene{
@@ -11,17 +10,13 @@ export default class Level1Scene extends Phaser.Scene{
         this.load.image('fondo','assets/fondo.png');
     }
     create(){
-        this.add.image(0,0,'fondo').setOrigin(0,0);
-        let player = new Player(this,this.scene.systems.game.scale.gameSize.width/2,this.scene.systems.game.scale.gameSize.height/2)
+        let bg = this.add.image(0,0,'fondo').setOrigin(0,0);
+        //align.scaleToGameW(bg, 2);
+        let player = new Player(this,this.scene.systems.game.scale.gameSize.width/2,this.scene.systems.game.scale.gameSize.height/2);
+        this.physics.world.setBounds(0,0,bg.width,bg.height);
+        this.cameras.main.setBounds(0, 0, bg.width+500, bg.height);
+        console.log(bg.width)
+        this.cameras.main.startFollow(player);
         
-        this.cameras.main.startFollow(this.player);
     }
-
-    enemyFollows () {
-		this.physics.moveToObject(this.goblin, this.player, 100);
-	}
-
-	update(){
-		this.enemyFollows();
-	}
 }
