2fd5e58acfe2fb1755b32914577960683c24440d
Daniel Castro Lopez <castrodaniel18@hotmail.com>
2023-04-28 01:58:54 +0200

    Subida de nivel con exp del suelo

--- src/objects/Potions/Potion.js
+++ src/objects/Potions/Potion.js
@@ -2,7 +2,6 @@ export default class Potion extends Phaser.GameObjects.Sprite {
     constructor(scene, spriteKey) {
         super(scene, Phaser.Math.Between(0, scene.game.config.width), Phaser.Math.Between(0, scene.game.config.height), spriteKey);
         this.setScale(.15);
-        //???????????
         this.scene.add.existing(this);
         this.scene.physics.add.existing(this, true);
     
--- src/objects/misc/ExperiencePoint.js
+++ src/objects/misc/ExperiencePoint.js
@@ -3,6 +3,17 @@ export default class ExperiencePoint extends Phaser.GameObjects.Sprite{
         super(scene, x, y, sprite);
         this.setScale(0.01);
         this.scene.add.existing(this);
-        //addCollisions
+        this.scene.physics.add.existing(this, true);
+    }
+
+    preUpdate() {
+        if (this.scene.physics.overlap(this.scene.player, this)) {
+            this.apply();
+            this.destroy();
+        }
+    }
+
+    apply(){
+        this.scene.player.playerExp += 25;
     }
 }
