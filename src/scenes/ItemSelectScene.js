a2c8ea34fc00a266ca7504ab6e173f5a04d427db
mzayas <mzboiza@gmail.com>
2023-05-02 12:57:07 +0200

    Ajustes botones subida nivel

--- src/scenes/ItemSelectScene.js
+++ src/scenes/ItemSelectScene.js
@@ -58,37 +58,141 @@ export default class ItemSelectScene extends Phaser.Scene{
         //crear los botones que contengan los objetos
         this.object1 = this.add.image(220, 300, elementosAleatorios[0].name + '_card').setInteractive();
         this.object1.setScale(.25);
-        this.object1.on('pointerdown', () => {
-            //Si queda espacio en el inventario o el objeto que se quiere coger ya se tiene
-            if(this.mainScene.playerItems.length < this.mainScene.player.inventorySize || this.mainScene.playerItems.some(item => item.name === elementosAleatorios[0].name)){
-                this.mainScene.waveController.leveling = false;
-                this.scene.resume(this.mainScene);
-                this.scene.stop();
-                this.itemSelect(elementosAleatorios[0]);
-            }
-        })
+        //Guardamos la escala original para usarla en el pointover
+        var originalScaleX = this.object1.scaleX;
+        var originalScaleY = this.object1.scaleY;
+        //Si el objeto no se puede seleccionar le ponemos un filtro rojo
+        if(this.mainScene.playerItems.length < this.mainScene.player.inventorySize || this.mainScene.playerItems.some(item => item.name === elementosAleatorios[0].name)){
+            this.object1.on('pointerdown', () => {
+                //Si queda espacio en el inventario o el objeto que se quiere coger ya se tiene
+                if(this.mainScene.playerItems.length < this.mainScene.player.inventorySize || this.mainScene.playerItems.some(item => item.name === elementosAleatorios[0].name)){
+                    this.mainScene.waveController.leveling = false;
+                    this.scene.resume(this.mainScene);
+                    this.scene.stop();
+                    this.itemSelect(elementosAleatorios[0]);
+                }
+            })
+            //Animación para tween
+            this.object1.on('pointerover', function () {
+                // Crea la animación tween
+                this.tweens.add({
+                targets: this.object1,
+                scaleX: 0.28, // Escala horizontal del botón
+                scaleY: 0.28, // Escala vertical del botón
+                alpha: 1, // Cambia la opacidad del botón a 0.7
+                duration: 200, // Duración de la animación en milisegundos
+                ease: 'Linear' // Tipo de interpolación de la animación
+                });
+    
+            }, this);
+        }else{
+            this.object1.setTint(0xff5252);
+        }
+
+        // Agrega un evento de puntero fuera del botón
+        this.object1.on('pointerout', function () {
+
+            // Crea la animación tween inversa
+            this.tweens.add({
+              targets: this.object1,
+              scaleX: originalScaleX, // Escala horizontal del botón de regreso al valor original
+              scaleY: originalScaleY, // Escala vertical del botón de regreso al valor original
+              alpha: 1, // Cambia la opacidad del botón a 0.7
+              duration: 200, // Duración de la animación en milisegundos
+              ease: 'Linear' // Tipo de interpolación de la animación
+            });
+  
+        }, this);
 
         this.object2 = this.add.image(400, 300, elementosAleatorios[1].name + '_card').setInteractive();
         this.object2.setScale(.25);
-        this.object2.on('pointerdown', () => {
-            if(this.mainScene.playerItems.length < this.mainScene.player.inventorySize || this.mainScene.playerItems.some(item => item.name === elementosAleatorios[1].name)){
-                this.mainScene.waveController.leveling = false;
-                this.scene.resume(this.mainScene);
-                this.scene.stop();
-                this.itemSelect(elementosAleatorios[1]);
-            }
-        })
+        if(this.mainScene.playerItems.length < this.mainScene.player.inventorySize || this.mainScene.playerItems.some(item => item.name === elementosAleatorios[1].name)){
+            originalScaleX = this.object2.scaleX;
+            originalScaleY = this.object2.scaleY;
+            this.object2.on('pointerdown', () => {
+                if(this.mainScene.playerItems.length < this.mainScene.player.inventorySize || this.mainScene.playerItems.some(item => item.name === elementosAleatorios[1].name)){
+                    this.mainScene.waveController.leveling = false;
+                    this.scene.resume(this.mainScene);
+                    this.scene.stop();
+                    this.itemSelect(elementosAleatorios[1]);
+                }
+            })
+            //Animación para tween
+            this.object2.on('pointerover', function () {
+                // Crea la animación tween
+                this.tweens.add({
+                targets: this.object2,
+                scaleX: 0.28, // Escala horizontal del botón
+                scaleY: 0.28, // Escala vertical del botón
+                alpha: 1, // Cambia la opacidad del botón a 0.7
+                duration: 200, // Duración de la animación en milisegundos
+                ease: 'Linear' // Tipo de interpolación de la animación
+                });
+    
+            }, this);
+        }else{
+            this.object2.setTint(0xff5252);
+        }
+
+        // Agrega un evento de puntero fuera del botón
+        this.object2.on('pointerout', function () {
+
+            // Crea la animación tween inversa
+            this.tweens.add({
+              targets: this.object2,
+              scaleX: originalScaleX, // Escala horizontal del botón de regreso al valor original
+              scaleY: originalScaleY, // Escala vertical del botón de regreso al valor original
+              alpha: 1, // Cambia la opacidad del botón a 0.7
+              duration: 200, // Duración de la animación en milisegundos
+              ease: 'Linear' // Tipo de interpolación de la animación
+            });
+  
+        }, this);
 
         this.object3 = this.add.image(580, 300, elementosAleatorios[2].name + '_card').setInteractive();
         this.object3.setScale(.25);
-        this.object3.on('pointerdown', () => {
-            if(this.mainScene.playerItems.length < this.mainScene.player.inventorySize || this.mainScene.playerItems.some(item => item.name === elementosAleatorios[2].name)){
-                this.mainScene.waveController.leveling = false;
-                this.scene.resume(this.mainScene);
-                this.scene.stop();
-                this.itemSelect(elementosAleatorios[2]);
-            }
-        })
+        if(this.mainScene.playerItems.length < this.mainScene.player.inventorySize || this.mainScene.playerItems.some(item => item.name === elementosAleatorios[2].name)){
+            originalScaleX = this.object3.scaleX;
+            originalScaleY = this.object3.scaleY;
+            this.object3.on('pointerdown', () => {
+                if(this.mainScene.playerItems.length < this.mainScene.player.inventorySize || this.mainScene.playerItems.some(item => item.name === elementosAleatorios[2].name)){
+                    this.mainScene.waveController.leveling = false;
+                    this.scene.resume(this.mainScene);
+                    this.scene.stop();
+                    this.itemSelect(elementosAleatorios[2]);
+                }
+            })
+            //Animación para tween
+            this.object3.on('pointerover', function () {
+                // Crea la animación tween
+                this.tweens.add({
+                targets: this.object3,
+                scaleX: 0.28, // Escala horizontal del botón
+                scaleY: 0.28, // Escala vertical del botón
+                alpha: 1, // Cambia la opacidad del botón a 0.7
+                duration: 200, // Duración de la animación en milisegundos
+                ease: 'Linear' // Tipo de interpolación de la animación
+                });
+    
+            }, this);
+        }else{
+            this.object3.setTint(0xff5252);
+        }
+
+        // Agrega un evento de puntero fuera del botón
+        this.object3.on('pointerout', function () {
+
+            // Crea la animación tween inversa
+            this.tweens.add({
+              targets: this.object3,
+              scaleX: originalScaleX, // Escala horizontal del botón de regreso al valor original
+              scaleY: originalScaleY, // Escala vertical del botón de regreso al valor original
+              alpha: 1, // Cambia la opacidad del botón a 0.7
+              duration: 200, // Duración de la animación en milisegundos
+              ease: 'Linear' // Tipo de interpolación de la animación
+            });
+  
+        }, this);
     }
 
     //Selecciona un item o lo sube de nivel, un mismo item solo puede llegar a nivel 5
